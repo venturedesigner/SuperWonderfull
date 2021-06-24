90dab73672b1f86f5daa54ab09c6141032cd158d
devance <charlie@devance.co>
2021-06-24 14:54:23 +0100

    Iteration 3 - Add a product

--- vue-shopping-cart/src/App.vue
+++ vue-shopping-cart/src/App.vue
@@ -21,19 +21,12 @@ export default {
     return {
       Products: ProductsData,
       UserProducts: [
-        {
-        "id": 1,
-        "name": "Banana",
-        "price": 1,
-        "imageUrl": "https://prodimage.images-bn.com/pimages/9781727333718_p0_v1_s550x406.jpg", 
-        "quantity": 3
-    }
-    // {
-    //     "id": 2,
-    //     "name": "Apple",
-    //     "price": 2,
-    //     "imageUrl": "http://images.hellokids.com/_uploads/_tiny_galerie/20120416/2gr_how-to-draw-an-apple-for-kids-tutorial-drawing.jpg" , 
-    //     "quantity": 4
+    //     {
+    //     "id": 1,
+    //     "name": "Banana",
+    //     "price": 1,
+    //     "imageUrl": "https://prodimage.images-bn.com/pimages/9781727333718_p0_v1_s550x406.jpg", 
+    //     "quantity": 3
     // }
       ]
     }
